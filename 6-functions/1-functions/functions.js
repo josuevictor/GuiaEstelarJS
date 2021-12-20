e4f15cda3ba2e3e3edb2a601f8b30d1f39ee6023
josuevictor-notebook <victorarroxellas01@gmail.com>
2021-12-20 20:00:03 -0300

    firts commit

---
+++ 6-functions/1-functions/functions.js
@@ -0,0 +1,16 @@
+//criar um aplicativo de frases motivacionais.
+
+// declaration - declaração da função.
+//function statament.
+function createPhrases(){  //guaradado em memória aguaradando ser executado.
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado.')
+}
+
+//Executar a fnção.
+//rodar, chamar, invocar .
+//execute run, call, invike.
+createPhrases()
+
+console.log('FIM')
---
+++ 6-functions/2-argumentos_e_parametros/functions1.js
@@ -0,0 +1,11 @@
+//função dentro de uma variável
+//function expression
+//function anonymous
+
+//parâmetros da função (parameters)
+const sum = function(number1, number2){
+    console.log(number1 + number2)
+
+}
+
+sum(2, 3)
