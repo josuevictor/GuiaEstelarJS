4417033a29a4ce7485e4f9eab151ab2cff9440ee
josuevictor-notebook <victorarroxellas01@gmail.com>
2021-12-20 21:49:05 -0300

    blablabla

--- 6-functions/2-argumentos_e_parametros/functions1.js
+++ 6-functions/2-argumentos_e_parametros/functions1.js
@@ -4,8 +4,21 @@
 
 //parâmetros da função (parameters)
 const sum = function(number1, number2){
-    console.log(number1 + number2)
+    let total = (number1 + number2) //declaração da variável sempre acompanhada do let. Caso contrário, causrá problemas com a var fora do escopo.
+    return total
 
 }
 
-sum(2, 3)
+let number1 = 34
+let number2 = 25
+
+//console.log(`o número 1 é ${number1}`)
+//console.log(`o número 2 é ${number2}`)
+console.log(`O total é ${sum(number1, number2)}`)
+console.log(total)
+
+
+//sum(2, 3) //Arguments - Argumentos
+
+/*===3-retornando_valores_dentro_da_funcao===*/ 
+
