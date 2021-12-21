c43764fd86af707465f795cf26c6f8fd3a1d1564
josuevictor-notebook <victorarroxellas01@gmail.com>
2021-12-21 13:18:23 -0300

    aula 7

---
+++ 7-arrow-function/functions.js
@@ -0,0 +1,9 @@
+//arrow function
+//jeito mais moderno de escrever uma função (mais compacta)
+//Nao sofre o hoisting
+
+const sayMyName = (name) => {
+    console.log(name)
+}
+
+sayMyName('Josué')
