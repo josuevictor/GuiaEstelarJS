bcfff8a889f00341695e422aa9ef0cd4fc082b6a
Josue Victor Arroxellas <josue.arroxellas@unimedmaceio.local>
2021-12-22 11:24:19 -0300

    cap. 7.2

---
+++ 7-manipulando-dados/2-type-conversion-coersion/scripts.js
@@ -0,0 +1,9 @@
+/*
+
+    Type conversion(usuário força a conversao) (typecasting) vs Type coersion(js força a conversão)
+
+    *Alteração de um tipo de dado para outro.
+
+*/
+
+console.log(Number('9') + 5)
